00d985c75c6e571adad14f89b6a222efa0afb71d
Timmy <Timmy@LADYKLUCK>
2023-01-25 23:02:32 +0100

    Add project files.

---
+++ dnd_character_generator/Character.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnd_character_generator
+{
+    internal class Character
+    {
+
+        public string CharName = string.Empty;
+        public string CharClass = string.Empty;
+        public string CharRace = string.Empty;
+
+        public Character()
+        {
+            this.CharName = "Ugluk";
+            RollClass();
+            RollRace();
+
+        }
+        public Character(string CharName)
+        {
+            this.CharName = CharName;
+
+            //this.CharClass = RollClass2();
+            RollClass();
+            RollRace();
+        }
+
+
+
+        public void RollClass()
+        {
+
+            int klass;
+            string CurrentClass = this.CharClass;
+
+            do
+            {
+                //Console.WriteLine("Genererar klass");
+                klass = RandomNumberGenerator.GetInt32(12);
+
+                switch (klass)
+                {
+                    case 0:
+                        CharClass = "Barbarian";
+                        break;
+                    case 1:
+                        CharClass = "Bard";
+                        break;
+                    case 2:
+                        CharClass = "Cleric";
+                        break;
+                    case 3:
+                        CharClass = "Druid";
+                        break;
+                    case 4:
+                        CharClass = "Fighter";
+                        break;
+                    case 5:
+                        CharClass = "Monk";
+                        break;
+                    case 6:
+                        CharClass = "Paladin";
+                        break;
+                    case 7:
+                        CharClass = "Ranger";
+                        break;
+                    case 8:
+                        CharClass = "Rogue";
+                        break;
+                    case 9:
+                        CharClass = "Sorcerer";
+                        break;
+                    case 10:
+                        CharClass = "Warlock";
+                        break;
+                    case 11:
+                        CharClass = "Wizard";
+                        break;
+
+                }
+            } while (CurrentClass == this.CharClass);
+        }
+
+
+        public void RollRace()
+        {
+            int ras;
+            string CurrentRace = this.CharRace;
+
+            do
+            {
+
+                //Console.WriteLine("Genererar ras...");
+                ras = RandomNumberGenerator.GetInt32(9);
+
+                switch (ras)
+            {
+                case 0:
+                    CharRace = "Dragonborn";
+                    break;
+                case 1:
+                    CharRace = "Dwarf";
+                    break;
+
+                case 2:
+                    CharRace = "Elf";
+                    break;
+                case 3:
+                    CharRace = "Gnome";
+                    break;
+                case 4:
+                    CharRace = "Half-Elf";
+                    break;
+                case 5:
+                    CharRace = "Halfling";
+                    break;
+                case 6:
+                    CharRace = "Half-Orc";
+                    break;
+                case 7:
+                    CharRace = "Human";
+                    break;
+                case 8:
+                    CharRace = "Tiefling";
+                    break;
+            }
+            }while(CurrentRace == this.CharRace);
+        }
+
+
+    }
+}
---
+++ dnd_character_generator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace dnd_character_generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Character Eynar = new Character("Eynar");
+
+
+            //Console.WriteLine("Namn: {0}, Klass: {1}, Ras: {2}", Eynar.CharName, Eynar.CharClass, Eynar.CharRace);
+
+            //Console.WriteLine("{0} vill inte vara {1}, rullar om klass", Eynar.CharName, Eynar.CharClass);
+
+            //string currentClass = Eynar.CharClass;
+            //do
+            //{
+            //    Console.WriteLine("rullar ny klass");
+            //    Eynar.RollClass();
+            //    Console.WriteLine(Eynar.CharClass);
+            //}
+            //while (Eynar.CharClass == currentClass) ;
+
+            
+            //Console.WriteLine("Namn: {0}, Klass: {1}, Ras: {2}", Eynar.CharName, Eynar.CharClass, Eynar.CharRace);
+
+            //Character Jovial = new Character("Jovial");
+
+            //    Console.WriteLine("Namn: {0}, Klass: {1}, Ras: {2}", Jovial.CharName, Jovial.CharClass, Jovial.CharRace);
+
+            Console.WriteLine("Vad vill du att din karaktär ska heta?");
+            string karaktar = Console.ReadLine();
+            Console.WriteLine("Genererar karaktär...");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("---------------------");
+            Character Char1 = new Character(karaktar);
+
+            //Console.WriteLine("Namn: {0}, Klass: {1}, Ras: {2}", Char1.CharName, Char1.CharClass, Char1.CharRace);
+            //Console.WriteLine(" ");
+            int choice = 100;
+            while(choice != 9)
+            {
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Namn: {0}, Klass: {1}, Ras: {2}", Char1.CharName, Char1.CharClass, Char1.CharRace);
+                Console.WriteLine(" ");
+                Console.WriteLine("Välj ett av följande: ");
+                Console.WriteLine("1: Godkänn karaktär");
+                Console.WriteLine("2: Byt klass på karaktär");
+                Console.WriteLine("3: Byt ras på karaktär");
+                Console.WriteLine("---------------------");
+
+                string input = Console.ReadLine();
+                if(!int.TryParse(input, out choice))
+                    {
+
+                    Console.WriteLine(choice);
+                    Console.WriteLine("Ogiltigt val, försök igen");
+
+                }
+                else if(choice > 3 || choice < 1)
+                {
+                    Console.WriteLine("Du måste väljar 1, 2 eller 3");
+                    choice = 100;
+                }
+                else if(choice == 1)
+                {
+                    Console.WriteLine("Karaktären godkänd! Avslutar programmet");
+                    choice = 9;
+                }
+                else if(choice == 2)
+                {
+                    Console.WriteLine("Genererar ny klass...");
+                    Char1.RollClass();
+                }
+                else if(choice == 3)
+                {
+                    Console.WriteLine("Generear ny ras");
+                    Char1.RollRace();
+                }
+
+            }
+
+
+        }
+    }
+}
